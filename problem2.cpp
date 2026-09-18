f3accac04f0f58d6e5fa6ff5052bb1bde99cc089
shadaaaalkharrr <shadaalkharwinny@gmail.com>
2026-09-18 12:46:51 +0700

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,67 +1,57 @@
 #include <iostream>
 
-// User-defined function untuk menghitung panjang karakter string (C-string)
-int customStrLen(const char str[]) {
+int hitungPanjang(char teks[]) {
     int len = 0;
-    while (str[len] != '\0') {
+    while (teks[len] != '\0') {
         len++;
     }
     return len;
 }
 
-// User-defined function untuk mengubah huruf kecil menjadi kapital
-char customToUpper(char c) {
+char keKapital(char c) {
     if (c >= 'a' && c <= 'z') {
         return c - 'a' + 'A';
     }
     return c;
 }
 
-// User-defined function untuk mengecek apakah karakter merupakan alfabet
-bool customIsAlpha(char c) {
-    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
-}
+int main() {
+    char pesan[500];
+    
+    std::cout << "Masukkan pesan rahasia: ";
+    std::cin >> pesan;
 
-// User-defined function untuk melakukan enkripsi enkripsi cipher
-void encryptAlienCipher(const char inputStr[], char outputStr[]) {
-    int length = customStrLen(inputStr);
-    int prevVal = 0;
+    int panjang = hitungPanjang(pesan);
+    int nilaiSebelumnya = 0;
 
-    for (int i = 0; i < length; i++) {
-        char currentChar = inputStr[i];
+    std::cout << "Pesan terenkripsi: ";
 
-        if (customIsAlpha(currentChar)) {
-            char upperChar = customToUpper(currentChar);
-            int currentVal = upperChar - 'A' + 1; // A=1, B=2, ..., Z=26
+    for (int i = 0; i < panjang; i++) {
+        char c = pesan[i];
+
+        if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+            char hurufBesar = keKapital(c);
+            int nilaiSekarang = hurufBesar - 'A' + 1;
 
             if (i == 0) {
-                outputStr[i] = upperChar;
+                std::cout << hurufBesar;
             } else {
-                int shiftedVal = currentVal + prevVal;
-                while (shiftedVal > 26) {
-                    shiftedVal -= 26;
+                int nilaiBaru = nilaiSekarang + nilaiSebelumnya;
+                
+                while (nilaiBaru > 26) {
+                    nilaiBaru -= 26;
                 }
-                outputStr[i] = (char)('A' + shiftedVal - 1);
+                
+                char hasil = 'A' + nilaiBaru - 1;
+                std::cout << hasil;
             }
-            // Simpan nilai huruf asli sebelumnya
-            prevVal = currentVal;
+            
+            nilaiSebelumnya = nilaiSekarang;
         } else {
-            outputStr[i] = currentChar;
+            std::cout << c;
         }
     }
-    outputStr[length] = '\0';
-}
 
-int main() {
-    char message[500];
-    char encrypted[500];
-
-    std::cout << "Masukkan pesan rahasia: ";
-    std::cin >> message;
-
-    encryptAlienCipher(message, encrypted);
-
-    std::cout << "Pesan terenkripsi: " << encrypted << std::endl;
-
+    std::cout << std::endl;
     return 0;
 }
